8277086161e18b4cf9d4b33d638b53282efc5638
Rahul Pandya <rapandya@microsoft.com>
2020-11-14 20:03:00 +0530

    fix

--- stats/data.py
+++ stats/data.py
@@ -3,7 +3,8 @@ import glob
 import pandas as pd
 from pandas.core.frame import DataFrame
 
-game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE')).sort()
+game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
+game_files.sort()
 
 game_frames = []
 for game_file in game_files:
